9e55724281663bc28d924d4572093739d3198d2f
worood test <Worood.test11@gmail.com>
2019-01-13 12:10:23 +0000

    changed the app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hi worood blah blah blah';
+  title = 'Welcome to my Angular application ! Hope you enjoy the journey! ';
 }
